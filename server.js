ca916c8a5a504373c5d9c432e8a2213e392df82c
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2024-07-01 09:16:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,39 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 app.get('/api/hello', async (req, res) => {
   const name = req.query.visitor_name;
-  // const clientIp=req.ip
-
+  //  const clientIp=req.ip;
   try {
     // Get location info based on IP
-     const token=process.env.TOKEN;
+    let clientIp;
+    if (req.headers['x-forwarded-for']) {
+      clientIp = req.headers['x-forwarded-for'].split(',')[0]; // Get first IP from comma-separated list (if present)
+    } else {
+      clientIp = req.socket.remoteAddress;
+    }
+    if (clientIp === '::1' || clientIp === '127.0.0.1') {
+      clientIp = '8.8.8.8'; // Use a well-known public IP address (Google's public DNS server)
+    }
+    console.log(`Client IP: ${clientIp}`);
+//  const clientIp =
+//     req.headers['X-Forwarded-For'] || req.socket.remoteAddress;
+   const token=process.env.TOKEN;
+  // const locationResponse = await axios.get(
+  //   `https://api.ipgeolocation.io/ipgeo?ip=${clientIp}`
+  // );
     const locationResponse = await axios.get(
-      `https://ipinfo.io/json?token=${token}`
+      `https://ipinfo.io/${clientIp}/json?token=${token}`
     );
-  
-    const clientIp=locationResponse.data.ip || '0.0.0.0';
-    const city = locationResponse.data.city || 'Bodrum'; 
+    
+    // const clientIp = locationResponse.data.ip || '0.0.0.0';
+    const cityLocation=locationResponse.data;
+    const city = cityLocation.city || 'unknown' ;
+      console.log(`City: ${city}`);
 
-   
     const weatherApiKey = process.env.WEATHER_API_KEY;
     const weatherResponse = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
@@ -32,7 +47,7 @@ app.get('/api/hello', async (req, res) => {
       greeting: `Hello, ${name}!, the temperature is ${cityTemp} degrees Celcius in ${city}`,
     });
   } catch (error) {
-    console.error('Error:',error.message);
+    console.error('Error:', error.message);
     res.status(500).send('Internal Server Error');
   }
 });
