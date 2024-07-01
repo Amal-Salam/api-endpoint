06bcc2e9e1f30c5b8cf80801b96bfa8e9f56daf6
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2024-07-01 09:34:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,34 +7,26 @@ dotenv.config();
 
 app.get('/api/hello', async (req, res) => {
   const name = req.query.visitor_name;
-  //  const clientIp=req.ip;
   try {
-    // Get location info based on IP
+    // Extracts location information based on the Ip address presented by router
     let clientIp;
     if (req.headers['x-forwarded-for']) {
-      clientIp = req.headers['x-forwarded-for'].split(',')[0]; // Get first IP from comma-separated list (if present)
+      clientIp = req.headers['x-forwarded-for'].split(',')[0];
     } else {
       clientIp = req.socket.remoteAddress;
     }
     if (clientIp === '::1' || clientIp === '127.0.0.1') {
-      clientIp = '8.8.8.8'; // Use a well-known public IP address (Google's public DNS server)
+      clientIp = '8.8.4.4'; 
     }
-    console.log(`Client IP: ${clientIp}`);
-//  const clientIp =
-//     req.headers['X-Forwarded-For'] || req.socket.remoteAddress;
+    
    const token=process.env.TOKEN;
-  // const locationResponse = await axios.get(
-  //   `https://api.ipgeolocation.io/ipgeo?ip=${clientIp}`
-  // );
     const locationResponse = await axios.get(
       `https://ipinfo.io/${clientIp}/json?token=${token}`
     );
     
-    // const clientIp = locationResponse.data.ip || '0.0.0.0';
     const cityLocation=locationResponse.data;
     const city = cityLocation.city || 'unknown' ;
-      console.log(`City: ${city}`);
-
+ 
     const weatherApiKey = process.env.WEATHER_API_KEY;
     const weatherResponse = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
